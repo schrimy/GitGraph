c9f6e7aadaf6891e4177cbe2f1ab0b9eba0a4fa1
Tom Schrimshaw <tschrimshaw@gmail.com>
2021-02-28 20:13:54 +0000

    chore: set up init files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import 'bootstrap/js/dist/util.js'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>,
+  </React>,
   document.getElementById('root')
 )
