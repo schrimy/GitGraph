054961c7a59694d168976e0afd3c4f0da4967f04
Tom Schrimshaw <tschrimshaw@gmail.com>
2021-03-01 14:27:48 +0000

    feat: grab GitHub data via GraphQL
    created helper method to make fetch call to GitHub graphQL to
    retrieve user's calendar contribuitons.

--- src/screens/SearchBar.js
+++ src/screens/SearchBar.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
+import { getGitData } from '../utils/helpers'
 
 const SearchBar = () => {
     //hold and update the form username
     const [userName, setUserName] = useState('')
 
+    //when a username is submitted this callback feeds the name to the helper method to fetch the data
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+
+        getGitData(userName)
+    }
+
     return (
-        <form className="container pt-2 pb-2">
+        <form className="container pt-2 pb-2" onSubmit={ handleSubmit } >
             <div className='form-group d-flex mb-0'>
                 <input
                     type="text"
---
+++ src/utils/helpers.js
@@ -0,0 +1,34 @@
+export const getGitData = (userName) => {
+    //grab token from hidden env file
+    const { REACT_APP_GIT_TOKEN } = process.env
+    //fetch promise call to GitHub graphQL to get passed in user's calendar contributions
+    fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        body: JSON.stringify({
+            query:
+                `query {
+                    user(login: "${userName}") {
+                        contributionsCollection {
+                            contributionCalendar {
+                                totalContributions
+                                weeks {
+                                    contributionDays {
+                                        color
+                                        contributionCount
+                                        date
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }`
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${REACT_APP_GIT_TOKEN}`
+        }
+    })
+    .then(data => data.json())
+    .then(res => console.log(res))
+    .catch(err => console.log('error getting GitHub data', err))
+}
