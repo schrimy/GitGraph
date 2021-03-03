1dcedc186541a53cadbf75b15a88edb6afb00809
Tom Schrimshaw <tschrimshaw@gmail.com>
2021-03-03 18:04:38 +0000

    feat: display 3d week contributions
    instead of example three.js, pull in the gitData and display
    a 3d block for each day in a week.
    Colour matches that from data and height is representation of
    number of commits.

--- src/containers/DataView.js
+++ src/containers/DataView.js
@@ -1,26 +1,58 @@
-import React, { useEffect, useRef } from 'react'
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three'
+import React, {
+    useEffect,
+    useRef
+} from 'react'
+import { connect } from 'react-redux'
+import {
+    Scene,
+    PerspectiveCamera,
+    WebGLRenderer,
+    BoxGeometry,
+    MeshBasicMaterial,
+    Mesh
+} from 'three'
 
-//example: https://blog.bitsrc.io/starting-with-react-16-and-three-js-in-5-minutes-3079b8829817
+//example used: https://blog.bitsrc.io/starting-with-react-16-and-three-js-in-5-minutes-3079b8829817
+//TODO: remove scene etc on unMount
+//TODO: or / and stop new scene being rendered on new search results
 
-const DataView = () => {
+const DataView = (props) => {
     let mount = useRef(null)
-    const scene = new Scene()
-    const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    const renderer = new WebGLRenderer()
-    const geometry = new BoxGeometry()
-    const material = new MeshBasicMaterial({ color: 0x00ff00 })
-    const cube = new Mesh(geometry, material)
+    const { contributions } = props
 
+    //TODO: change width / height of camera perspective
+    //three.js variables
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    const renderer = new WebGLRenderer()
+
+    //useEffect to setup and render 3d scene when component mounted
     useEffect(() => {
+        console.log('contributions:', contributions)
+        if(contributions !== null) {
+            setScene(contributions)
+        }
+    })
+
+    const setScene = (contribs) => {
         renderer.setSize(window.innerWidth, window.innerHeight)
         mount.appendChild(renderer.domElement)
 
-        scene.add(cube)
+        for(const [i, day] of contribs.entries()) {
+            console.log('add cube')
+            const geometry = new BoxGeometry(1, (1 + day.contributionCount/2), 1)
+            const material = new MeshBasicMaterial({ color: day.color })
+            const cube = new Mesh(geometry, material)
+            cube.position.x += (i*2) - contribs.length
+            cube.position.y = 1 + (day.contributionCount/2)/2
+            scene.add(cube)
+        }
 
-        camera.position.z = 5
+        camera.position.z = 10
+        camera.position.y = 5
+        renderer.render( scene, camera )
 
-        const animate = () => {
+        /*const animate = () => {
             requestAnimationFrame( animate )
     
             cube.rotation.x += 0.01
@@ -29,12 +61,19 @@ const DataView = () => {
             renderer.render( scene, camera )
         }
 
-        animate()
-    })
+        animate()*/
+    }
 
     return (
         <div ref={ ref => (mount = ref) }></div>
     )
 }
 
-export default DataView
+//for now just return first week of contributions, so not to break processing
+function mapStateToProps({ gitData }) {
+    return {
+        contributions: gitData !== null ? gitData.weeks[0].contributionDays : null
+    }
+}
+
+export default connect(mapStateToProps)(DataView)
